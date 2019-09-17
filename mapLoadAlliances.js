b99533f7f46cb4f9c0aa93e57c6f6174ad836ae0
Shuri2060 <jeremyho2060@gmail.com>
2019-09-17 22:13:00 +0100

    Update mapLoadAlliances.js

--- mapLoadAlliances.js
+++ mapLoadAlliances.js
@@ -1,10 +1,10 @@
-$('img[src="img/del.gif"]').parent().parent().remove()
+$('img[src="img/del.gif"]').parent().parent().remove();
 
-allies = []
+allies = [];
 
-var alliesList = [["61","#4986e7"],["1","#3c3c3c"],["14","#cc00cc"],["170","#f691b2"],["253","#fad165"],["60","#7bd148"],["32","#ff7537"],["194","#ff7537"],["367","#f691b2"],["37","#ff0000"],["33","#33ffff"],["423","#33ffff"],["378","#ffffff"]]
+var alliesList = [["61","#4986e7"],["1","#3c3c3c"],["14","#cc00cc"],["170","#f691b2"],["253","#fad165"],["60","#7bd148"],["32","#ff7537"],["194","#ff7537"],["367","#f691b2"],["37","#ff0000"],["33","#33ffff"],["423","#33ffff"],["378","#ffffff"]];
 
 for (i = alliesList .length; i--;) {
-  const ally = $('option[value=' + alliesList[i][0] + ']')
-  addAlly(alliesList[i][0], alliesList[i][1], ally[ally.length - 1].text)
+  const ally = $('option[value=' + alliesList[i][0] + ']');
+  addAlly(alliesList[i][0], alliesList[i][1], ally[ally.length - 1].text);
 }
